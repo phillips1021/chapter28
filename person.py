7ed5dcfb248ae478f9785257abceb37bf4236112
bphillips <bphillips@cboe.com>
2019-02-14 09:30:29 -0600

    Added a static method

--- person.py
+++ person.py
@@ -5,10 +5,13 @@ class Person:
 
     company_name = "Cboe"
 
+    number_of_people = 0
+
     def __init__(self, name, job=None, pay=0):
         self.name = name
         self.job = job
         self.pay = pay
+        Person.number_of_people += 1
 
     def last_name(self):
         return self.name.split()[-1]
@@ -21,5 +24,11 @@ class Person:
         """
         self.pay = int(self.pay * (1 + percent))
 
+    def get_number_of_people():
+        return Person.number_of_people
+
+    get_number_of_people = staticmethod(get_number_of_people)
+
     def __repr__(self):
-        return '[Person: %s %s %s %s]' % (Person.company_name, self.name, self.job, self.pay)
+        return '[Person: %s %s %s %s - Total People: %s]' \
+               % (Person.company_name, self.name, self.job, self.pay, Person.get_number_of_people())
